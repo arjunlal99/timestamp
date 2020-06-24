75b93d5679b21423e8df2994b344a3cec578895d
arjunlal99 <32013318+arjunlal99@users.noreply.github.com>
2020-06-24 06:21:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/", function (req,res){
+  var date = new Date();
+  res.json({"unix":Date.parse(date), "utc":date.toUTCString()})
+})
+
+app.get("/api/timestamp/:date_string", function(req, res){
+  var dateString = (req.params.date_string);
+  
+  if (/\d{5,}/.test(dateString)){
+    dateInt = parseInt(dateString);
+    res.json({unix:dateInt,utc:new Date(dateInt).toUTCString()})
+  }
+
+  let date = new Date(dateString);
+
+  if (date.toString() == "Invalid Date"){
+    res.json({error: "Invalid Date"});
+  }
+  else{
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+})
+
+// listen for requests :)
+var listener = app.listen(8080, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
